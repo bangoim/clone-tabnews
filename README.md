13f08c6288e8ebaad14edfc62bceec3d3b9102cc
João Castello Branco <129008919+bangoim@users.noreply.github.com>
2024-11-26 19:20:08 +0000

    adciciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tab-news
+
 Esse repositório serve apenas para replicar as atividades passadas no curso.dev do Filipe Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Texto alterado.</h1>
+function Home() {
+  return <h1>Texto alterado.</h1>;
+}
+
+function teste() {
+  console.log("identação errada");
 }
 
 export default Home;
